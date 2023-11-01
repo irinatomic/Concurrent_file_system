a6666ed134c0650c0ab7a9f70fd0587f68df73fd
Irina Tomic <irina.z.tomic@gmail.com>
2023-11-01 22:44:09 +0100

    Add get file

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import threading
 import hashlib
 import zlib
@@ -5,79 +6,128 @@ import yaml
 import sys
 import os
 
+# Global dictionaries to store file and part information
 file_register = {}
 parts_register = {}
+
+# Constants for the file and part register
+READY = "ready"
+FILENAME = "filename"
+MD5_HASH = "md5_hash"
+PARTS_COUNT = "parts_count"
+
+# Constants from the yaml file
+SYSTEM = "system"
+STORAGE = "storage"
+PART_SIZE = "part_size"
+IO_PROCESSES = "io_processes"
+PARTS_DIRECTORY = "parts_directory"
+SAVED_DIRECTORY = "saved_directory"
 
 with open('config.yaml', 'r') as config_file:
     config = yaml.safe_load(config_file)
 
+# I/O PROCESSES
 def process_file_part(part_id, part_data):
     md5_hash = hashlib.md5(part_data).hexdigest()
     compressed_data = zlib.compress(part_data)
-    # write the compressed data to a file
-    with open(os.path.join(config['storage']['parts_directory'], f"{part_id}.dat"), 'wb') as writer:
+
+    # Write the compressed data to a file
+    with open(os.path.join(config[STORAGE][PARTS_DIRECTORY], f"{part_id}.dat"), 'wb') as writer:
         writer.write(compressed_data)
+
     return part_id, md5_hash
 
+# I/O PROCESSES 
+def get_file_part(part_id):
+    # Read the compressed data from a file
+    with open(os.path.join(config[STORAGE][PARTS_DIRECTORY], f"{part_id}.dat"), 'rb') as reader:
+        compressed_data = reader.read()
+        part_data = zlib.decompress(compressed_data)
+        md5_hash_check = hashlib.md5(part_data).hexdigest()
+        
+        if md5_hash_check == parts_register[part_id][MD5_HASH]:
+            return part_data
+    return None
+
+# PUT
 def put(filename):
     file_id = len(file_register)
-    file_register[file_id] = {"filename": filename, "ready": False}
-    part_size = config['system']['part_size']
+    file_register[file_id] = {FILENAME: filename, READY: False}
+    part_size = config[SYSTEM][PART_SIZE]   # Part size is 1 MB
 
     with open(filename, 'rb') as file:
         part_index = 0
         while True:
             part_data = file.read(part_size)
             if not part_data:
-                break                               # EOF
-            
+                break  # EOF
+
             part_id = f"{file_id}_{part_index}"
-            parts_register[part_id] = {"ready": False}
+            parts_register[part_id] = {READY: False}
 
             part_id, md5_hash = process_file_part(part_id, part_data)
-            parts_register[part_id]["md5_hash"] = md5_hash
-            parts_register[part_id]["ready"] = True
+            parts_register[part_id][MD5_HASH] = md5_hash
+            parts_register[part_id][READY] = True
             part_index += 1
 
     # Check if the whole file is ready
-    if all(parts_register[f"{file_id}_{i}"]["ready"] for i in range(part_index)):
-        file_register[file_id]["ready"] = True
+    if all(parts_register[f"{file_id}_{i}"][READY] for i in range(part_index)):
+        file_register[file_id][READY] = True
+        file_register[file_id][PARTS_COUNT] = part_index
 
+# GET
+def get(file_id):
+    file_id = int(file_id)
+    if file_id in file_register and file_register[file_id][READY]:
+        parts_count = file_register[file_id][PARTS_COUNT]
+        new_filename = f"new_file_{file_id}_{datetime.timestamp(datetime.now())}.txt"
+        new_filepath = os.path.join(config[STORAGE][SAVED_DIRECTORY], new_filename)
+        part_index = 0
 
-def get(filename):
-    sys.stdout.write("getting file: " + filename + "\n")
+        with open(new_filepath, 'wb') as writer:
+            for i in range(parts_count):
+                part_id = f"{file_id}_{i}"
+                if part_id in parts_register and parts_register[part_id][READY]:
+                    part_data = get_file_part(part_id)
+                    if not part_data:
+                        sys.stdout.write(f"Error: Part {part_id} is corrupted\n")
+                        break
+                    writer.write(part_data)
+                    part_index += 1
+                else:
+                    break
 
+# DELETE
 def delete(filename):
-    sys.stdout.write("deleting file: " + filename + "\n")
+    sys.stdout.write("Deleting file: " + filename + "\n")
 
+# LIST
 def list_files():
-    sys.stdout.write("listing files\n")
+    for file_id in file_register:
+        sys.stdout.write(f"{file_id}: {file_register[file_id][FILENAME]}\n")
 
-# Delete all files in the parts directory and saved directory
+# Function to delete extra files:
+#  - Files in the parts directory
+#  - Files in the saved directory
 def delete_extra_files():
-    parts_directory = config['storage']['parts_directory']
-    if os.path.exists(parts_directory) and os.path.isdir(parts_directory):
-        files = os.listdir(parts_directory)
-        for file in files:
-            file_path = os.path.join(parts_directory, file)
-            if os.path.isfile(file_path):
-                os.remove(file_path)
-            else:
-                sys.stdout.write(f"Skipping non-file: {file_path}")
-    else:
-        sys.stdout.write(f"Directory does not exist: {parts_directory}")
+    def delete_files_in_directory(directory):
+        if os.path.exists(directory) and os.path.isdir(directory):
+            files = os.listdir(directory)
+            for file in files:
+                file_path = os.path.join(directory, file)
+                if os.path.isfile(file_path):
+                    os.remove(file_path)
+                else:
+                    sys.stdout.write(f"Skipping non-file: {file_path}")
+        else:
+            sys.stdout.write(f"Directory does not exist: {directory}")
 
-    saved_directory = config['storage']['saveddirectory']
-    if os.path.exists(saved_directory) and os.path.isdir(saved_directory):
-        files = os.listdir(saved_directory)
-        for file in files:
-            file_path = os.path.join(saved_directory, file)
-            if os.path.isfile(file_path):
-                os.remove(file_path)
-            else:
-                sys.stdout.write(f"Skipping non-file: {file_path}")
-    else:
-        sys.stdout.write(f"Directory does not exist: {saved_directory}")
+    parts_directory = config[STORAGE][PARTS_DIRECTORY]
+    saved_directory = config[STORAGE][SAVED_DIRECTORY]
+
+    delete_files_in_directory(parts_directory)
+    delete_files_in_directory(saved_directory)
 
 def main():
     threads = []
