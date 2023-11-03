bd7c3e6942148e22982b1fe52d8b1e4a6935c95c
Irina Tomic <irina.z.tomic@gmail.com>
2023-11-03 01:36:18 +0100

    Add get function (uses threads pool for I/O)

--- run.py
+++ run.py
@@ -10,6 +10,13 @@ import os
 # Global dictionaries to store file and part information
 file_register = {}
 parts_register = {}
+file_counter = 0
+
+# Locks for the file and part register
+# Will change in the future so for every element in the dictionary there is a lock
+# For now, only one lock for the whole dictionary
+file_register_lock = threading.Lock()
+parts_register_lock = threading.Lock()
 
 # Constants for the file and part register
 READY = "ready"
@@ -28,30 +35,36 @@ SAVED_DIRECTORY = "saved_directory"
 with open('config.yaml', 'r') as config_file:
     config = yaml.safe_load(config_file)
 
-part_size = config[SYSTEM][PART_SIZE]  # Part size is 1 MB
+part_size = config[SYSTEM][PART_SIZE]                   # Part size is 1 MB
+batch_size = 4                                          # Batch size for processing parts in parallel
 
 # I/O PROCESSES
-def process_file_part(arguments):
-    part_id, part_data = arguments
+def put_file_part(arguments):
+    full_part_id, part_data = arguments
     md5_hash = hashlib.md5(part_data).hexdigest()
     compressed_data = zlib.compress(part_data)
 
     # Write the compressed data to a file
-    with open(os.path.join(config[STORAGE][PARTS_DIRECTORY], f"{part_id}.dat"), 'wb') as writer:
+    with open(os.path.join(config[STORAGE][PARTS_DIRECTORY], f"{full_part_id}.dat"), 'wb') as writer:
         writer.write(compressed_data)
 
-    return part_id, md5_hash
+    return full_part_id, md5_hash
 
 # I/O PROCESSES 
-def get_file_part(part_id):
-    # Read the compressed data from a file
-    with open(os.path.join(config[STORAGE][PARTS_DIRECTORY], f"{part_id}.dat"), 'rb') as reader:
-        compressed_data = reader.read()
-        part_data = zlib.decompress(compressed_data)
-        md5_hash_check = hashlib.md5(part_data).hexdigest()
-        
-        if md5_hash_check == parts_register[part_id][MD5_HASH]:
-            return part_data
+def get_file_part(arguments):
+    global parts_register
+    full_part_id, previous_md5_hash = arguments
+    file_path = os.path.join(config[STORAGE][PARTS_DIRECTORY], f"{full_part_id}.dat")
+
+    if os.path.exists(file_path):
+        with open(file_path, 'rb') as reader:
+            compressed_data = reader.read()
+            part_data = zlib.decompress(compressed_data)
+            md5_hash_check = hashlib.md5(part_data).hexdigest()
+
+            if md5_hash_check == previous_md5_hash:
+                return part_data
+
     return None
 
 # I/O PROCESSES 
@@ -64,14 +77,17 @@ def delete_file_part(part_filename: str) -> bool:
         return False
 
 # PUT
-def put(filename, io_pool):
-    file_id = len(file_register)
+def put(filename, io_pool, file_id):
+    global file_register
+    global parts_register
+
+    # BICE SAMO LOCK JEDNOG ELEMENTA IZ D
+    file_register_lock.acquire()
     file_register[file_id] = {FILENAME: filename, READY: False}
 
     with open(filename, 'rb') as file:
         num_parts = (os.path.getsize(filename) + part_size - 1) // part_size
         part_ids = list(range(num_parts))
-        batch_size = 4
 
         for i in range(0, num_parts, batch_size):
             parts_data_tuples = []                                  # List of tuples (part_id, part_data)
@@ -83,7 +99,7 @@ def put(filename, io_pool):
                 parts_data_tuples.append((f"{file_id}_{part_id}", part_data))
 
             # Process the parts in parallel
-            results = io_pool.map(process_file_part, parts_data_tuples)
+            results = io_pool.map(put_file_part, parts_data_tuples)
 
             # Write the results to the parts register
             for full_part_id, md5_hash in results:
@@ -92,29 +108,49 @@ def put(filename, io_pool):
     if all(parts_register[f"{file_id}_{i}"][READY] for i in range(num_parts)):
         file_register[file_id][READY] = True
         file_register[file_id][PARTS_COUNT] = num_parts
-
+    file_register_lock.release()
 
 # GET
 def get(file_id_arg):
+    global file_register
+    global parts_register
+
     file_id = int(file_id_arg)
-    if file_id in file_register and file_register[file_id][READY]:
-        parts_count = file_register[file_id][PARTS_COUNT]
+
+    file_register_lock.acquire()
+    file_ready = file_register[file_id][READY]
+
+    if file_id in file_register and file_ready:
+        num_parts = file_register[file_id][PARTS_COUNT]
+        part_ids = list(range(num_parts))
+
+        # Create a file to write in
         new_filename = f"new_file_{file_id}_{datetime.timestamp(datetime.now())}.txt"
         new_filepath = os.path.join(config[STORAGE][SAVED_DIRECTORY], new_filename)
-        part_index = 0
 
         with open(new_filepath, 'wb') as writer:
-            for i in range(parts_count):
-                part_id = f"{file_id}_{i}"                                              # part_id = filename of the saved part
-                if part_id in parts_register and parts_register[part_id][READY]:
-                    part_data = get_file_part(part_id)
+            for i in range(0, num_parts, batch_size):
+                parts_data_tuples = []                              # List of tuples (part_id, md5_hash)
+                part_ids_batch = part_ids[i:i + batch_size]
+                for part_id in part_ids_batch:
+                    full_part_id = f"{file_id}_{part_id}"
+                    parts_register_lock.acquire()
+                    md5_hash = parts_register[full_part_id][MD5_HASH]
+                    parts_register_lock.release()
+                    parts_data_tuples.append((full_part_id, md5_hash))
+
+                # Process the parts in parallel
+                results = io_pool.map(get_file_part, parts_data_tuples)
+
+                # If the part is corrupted, the part_data is None
+                for part_data in results:
                     if not part_data:
-                        sys.stdout.write(f"Error: Part {part_id} is corrupted\n")
-                        break
+                        sys.stdout.write(f"Error: Some parts are missing or corrupted\n")
+                        file_register_lock.release()
+                        return
                     writer.write(part_data)
-                    part_index += 1
-                else:
-                    break
+
+    file_register_lock.release()
 
 # DELETE
 def delete(file_id_arg):
@@ -152,9 +188,16 @@ def delete(file_id_arg):
 
 # LIST
 def list_files():
+    file_register_lock.acquire()
     for file_id in file_register:
         if file_register[file_id][READY]:
             sys.stdout.write(f"{file_id}: {file_register[file_id][FILENAME]}\n")
+    file_register_lock.release()
+
+    parts_register_lock.acquire()
+    for key in parts_register.keys():
+            sys.stdout.write(f"{key}: {parts_register[key]}\n")
+    parts_register_lock.release()
 
 # Function to delete extra files:
 #  - Files in the parts directory
@@ -179,6 +222,7 @@ def delete_extra_files():
     delete_files_in_directory(saved_directory)
 
 def main(io_pool):
+    global file_counter
     threads = []
 
     while True:
@@ -188,7 +232,8 @@ def main(io_pool):
         action = parts[0]
 
         if action == "put" and len(parts) == 2:
-            t = threading.Thread(target=put, args=(parts[1], io_pool))
+            file_counter += 1
+            t = threading.Thread(target=put, args=(parts[1], io_pool, file_counter))
             t.start()
             threads.append(t)
         elif action == "get" and len(parts) == 2:
